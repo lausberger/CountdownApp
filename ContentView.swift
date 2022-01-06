0ed6ce31c79bd225bb2113aa4d542138d7ac4d3a
Lucas Ausberger <74087830+lausberger@users.noreply.github.com>
2022-01-05 19:33:58 -0600

    support for DeleteConfirmation and restructuring

--- ContentView.swift
+++ ContentView.swift
@@ -15,14 +15,17 @@ struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @Environment(\.colorScheme) var colorScheme
     @State var creatingEvent = false
-    
     @State var editing = false
+    @State var deleting = false
     @State var ETE: EventToEdit = EventToEdit()
     
     var body: some View {
         ZStack {
             NavigationView {
-                EventListView(editing: $editing, eventHolder: $ETE)
+                EventListView(
+                    editing: $editing,
+                    deleting: $deleting,
+                    eventHolder: $ETE)
                 .listStyle(.plain)
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
@@ -41,36 +44,41 @@ struct ContentView: View {
                 }
                 .navigationTitle("Events")
             }
-            if creatingEvent {
+            if creatingEvent || editing || deleting {
                 Rectangle()
                     .foregroundColor(
                         colorScheme == .dark ?
                             Color.white.opacity(0.25) :
                             Color.black.opacity(0.5))
                     .edgesIgnoringSafeArea(.all)
-                VStack {
-                    EventBuilder(active: $creatingEvent)
-                }
-            } else if editing {
-                Rectangle()
-                    .foregroundColor(
-                        colorScheme == .dark ?
-                            Color.white.opacity(0.25) :
-                            Color.black.opacity(0.5))
-                    .edgesIgnoringSafeArea(.all)
-                if ETE.get != nil {
-                    EventEditor(
-                        event: ETE.get!,
-                        newDate: ETE.get!.date!,
-                        newTime: ETE.get!.date!.timeStamp()!,
-                        newName: ETE.get!.name!,
-                        newAllDay: ETE.get!.isAllDay,
-                        editing: $editing)
-                } else {
-                    Text("Failed to edit event")
-                        .background(Color.red)
+                if creatingEvent {
+                    VStack {
+                        EventBuilder(active: $creatingEvent)
+                    }
+                } else if editing {
+                    if ETE.get != nil {
+                        EventEditor(
+                            event: ETE.get!,
+                            newDate: ETE.get!.date!,
+                            newTime: ETE.get!.date!.timeStamp()!,
+                            newName: ETE.get!.name!,
+                            newAllDay: ETE.get!.isAllDay,
+                            editing: $editing)
+                    } else {
+                        Text("Failed to edit event")
+                            .background(Color.red)
+                    }
+                } else if deleting {
+                    if ETE.get != nil {
+                        DeleteConfirmation(deleting: $deleting, event: ETE.get!)
+                            
+                    } else {
+                        Text("Failed to delete event")
+                            .background(Color.red)
+                    }
                 }
             }
+            
         }
     }
 }
